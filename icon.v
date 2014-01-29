b6b1cc18508ed121db504c7d98aefa9dbf5f31d3
D Bhavana <batbhavana@gmail.com>
2014-01-28 22:05:36 -0800

    icon.v with correct orientation and good allignment..

--- icon.v
+++ icon.v
@@ -39,6 +39,7 @@ output reg [1:0] icon
 reg [1:0] bitmap_bot_1 [0:15] [0:15];
 reg [1:0] bitmap_bot_2 [0:15] [0:15];
 integer i,j;
+reg [9:0] locX,locY;
 
 always @(*) begin
 	for (i=0; i<=15; i=i+1) begin
@@ -72,31 +73,34 @@ always @ (posedge clock) begin
 		icon <= 2'b00;
 	end
 	else begin
-		if ((Pixel_row >= ({LocY_reg,2'b00})) && (Pixel_row <= (({LocY_reg,2'b00}) + 4'hF)) && (Pixel_column >= ({LocX_reg,2'b00})) && (Pixel_column <= (({LocX_reg,2'b00}) + 4'hF)) ) begin
+		locX = ({LocX_reg,2'b00} - 4'b1001);
+		locY = ({LocY_reg,2'b00} - 3'b100);
+		if ((Pixel_row >= locY) && (Pixel_row <= (locY + 4'hF)) && (Pixel_column >= locX) && (Pixel_column <= (locX + 4'hF)) ) begin
+			
 			case(BotInfo_reg[2:0])
-				3'b000 : begin 
-							icon <= bitmap_bot_1 [Pixel_row - ({LocY_reg,2'b00})] [Pixel_column - ({LocX_reg,2'b00})];	//0 Degree
-						end	
-				3'b001 : begin 
-							icon <= bitmap_bot_2 [Pixel_row - ({LocY_reg,2'b00})] [Pixel_column - ({LocX_reg,2'b00})];	//45 Degree
-						end	
-				3'b010 : begin 
-							icon <= bitmap_bot_1 [Pixel_column - ({LocX_reg,2'b00})] [({LocY_reg,2'b00}) + 4'hF - Pixel_row];	//90 Degree
-						end	
-				3'b011 : begin 
-							icon <= bitmap_bot_2 [Pixel_column - ({LocX_reg,2'b00})] [({LocY_reg,2'b00}) + 4'hF - Pixel_row];	//135 Degree
-						end	
-				3'b100 : begin 
-							icon <= bitmap_bot_1 [({LocY_reg,2'b00}) + 4'hF - Pixel_row] [({LocX_reg,2'b00}) + 4'hF - Pixel_column];	//180 Degree
+				3'b110 : begin 
+							icon <= bitmap_bot_1 [Pixel_row - locY] [Pixel_column - locX];	//0 Degree
 						end	
 				3'b101 : begin 
-							icon <= bitmap_bot_2 [({LocY_reg,2'b00}) + 4'hF - Pixel_row] [({LocX_reg,2'b00}) + 4'hF - Pixel_column];	//225 Degree
+							icon <= bitmap_bot_2 [Pixel_row - locY] [Pixel_column - locX];	//45 Degree
 						end	
-				3'b110 : begin 
-							icon <= bitmap_bot_1 [({LocX_reg,2'b00}) + 4'hF - Pixel_column] [Pixel_row - ({LocY_reg,2'b00})];	//270 Degree
+				3'b100 : begin 
+							icon <= bitmap_bot_1 [Pixel_column - locX] [locY + 4'hF - Pixel_row];	//90 Degree
+						end	
+				3'b011 : begin 
+							icon <= bitmap_bot_2 [Pixel_column - locX] [locY + 4'hF - Pixel_row];	//135 Degree
+						end	
+				3'b010 : begin 
+							icon <= bitmap_bot_1 [locY + 4'hF - Pixel_row] [locX + 4'hF - Pixel_column];	//180 Degree
+						end	
+				3'b001 : begin 
+							icon <= bitmap_bot_2 [locY + 4'hF - Pixel_row] [locX + 4'hF - Pixel_column];	//225 Degree
+						end	
+				3'b000 : begin 
+							icon <= bitmap_bot_1 [locX + 4'hF - Pixel_column] [Pixel_row - locY];	//270 Degree
 						end	
 				3'b111 : begin 
-							icon <= bitmap_bot_2 [({LocX_reg,2'b00}) + 4'hF - Pixel_column] [Pixel_row - ({LocY_reg,2'b00})];	//315 Degree
+							icon <= bitmap_bot_2 [locX + 4'hF - Pixel_column] [Pixel_row - locY];	//315 Degree
 						end			
 		    endcase
 		end
